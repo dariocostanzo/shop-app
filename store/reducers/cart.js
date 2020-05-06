2d5eb2d65a39ea3b968a8cc7f4d829d8a184fa77
dariocostanzo <darioc@xara.com>
2020-05-06 12:19:52 +0100

    Adding Logic to delete items

--- store/reducers/cart.js
+++ store/reducers/cart.js
@@ -1,4 +1,4 @@
-import { ADD_TO_CART } from '../action/cart';
+import { ADD_TO_CART, REMOVE_FROM_CART } from '../action/cart';
 import CartItem from '../../models/cart-item';
 
 const initialState = {
@@ -38,6 +38,28 @@ export default (state = initialState, action) => {
           totalAmount: state.totalAmount + prodPrice
         };
       }
+    case REMOVE_FROM_CART:
+      const selectedCartItem = state.items[action.pid];
+      const currentQty = selectedCartItem.quantity;
+      let updatedCartItems;
+      if (currentQty > 1) {
+        // need to reduce it, not erase it
+        const updatedCartItem = new CartItem(
+          selectedCartItem.quantity - 1,
+          selectedCartItem.productPrice,
+          selectedCartItem.productTitle,
+          selectedCartItem.sum - selectedCartItem.productPrice
+        );
+        updatedCartItems = { ...state.items, [action.pid]: updatedCartItem };
+      } else {
+        updatedCartItems = { ...state.items };
+        delete updatedCartItems[action.pid];
+      }
+      return {
+        ...state,
+        items: updatedCartItems,
+        totalAmount: state.totalAmount - selectedCartItem.productPrice
+      };
   }
   return state;
 };
