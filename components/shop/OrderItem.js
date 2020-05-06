cd1f7deb62bd006a15a44b1a9639a93285740ff8
dariocostanzo <darioc@xara.com>
2020-05-06 16:43:04 +0100

    Making the "Show Details" button work

--- components/shop/OrderItem.js
+++ components/shop/OrderItem.js
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
 
 import Colors from '../../constants/Colors';
 import CartItem from './CartItem';
 
 const OrderItem = props => {
+  const [showDetails, setShowDetails] = useState(false);
   return (
     <View style={styles.orderItem}>
       <View style={styles.summary}>
         <Text style={styles.totalAmount}>${props.amount.toFixed(2)}</Text>
         <Text style={styles.date}>{props.date}</Text>
       </View>
-      <Button color={Colors.primary} title='Show Details' />
+      <Button
+        color={Colors.primary}
+        title={showDetails ? 'Hide Details' : 'Show Details'}
+        onPress={() => {
+          setShowDetails(prevState => !prevState);
+        }}
+      />
+      {showDetails && (
+        <View style={styles.detailItem}>
+          {props.items.map(cartItem => (
+            <CartItem
+              key={cartItem.productId}
+              quantity={cartItem.quantity}
+              amount={cartItem.sum}
+              title={cartItem.productTitle}
+            />
+          ))}
+        </View>
+      )}
     </View>
   );
 };
@@ -44,6 +63,9 @@ const styles = StyleSheet.create({
     fontFamily: 'open-sans',
     fontSize: 16,
     color: '#888'
+  },
+  detailItem: {
+    width: '100%'
   }
 });
 
