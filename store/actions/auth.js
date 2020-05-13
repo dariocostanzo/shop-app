3093af257e5c241f76c2e7be76229a2fb5552a29
dariocostanzo <darioc@xara.com>
2020-05-13 12:37:55 +0100

    Logging Users In

--- store/actions/auth.js
+++ store/actions/auth.js
@@ -1,11 +1,10 @@
 export const SIGNUP = 'SIGNUP';
+export const LOGIN = 'LOGIN';
 
 export const signup = (email, password) => {
   return async dispatch => {
     const response = await fetch(
       'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCmXqcJVGuLzS6NPu7Zke-Rx5b6dj924_Y',
-
-      // request to create a new user
       {
         method: 'POST',
         headers: {
@@ -18,6 +17,7 @@ export const signup = (email, password) => {
         })
       }
     );
+
     if (!response.ok) {
       throw new Error('Something went wrong!');
     }
@@ -25,5 +25,32 @@ export const signup = (email, password) => {
     const resData = await response.json();
     console.log(resData);
     dispatch({ type: SIGNUP });
+  };
+};
+
+export const login = (email, password) => {
+  return async dispatch => {
+    const response = await fetch(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCmXqcJVGuLzS6NPu7Zke-Rx5b6dj924_Y',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+          returnSecureToken: true
+        })
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error('Something went wrong!');
+    }
+
+    const resData = await response.json();
+    console.log(resData);
+    dispatch({ type: LOGIN });
   };
 };
