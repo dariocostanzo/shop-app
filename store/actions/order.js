7da32ad3cce90d3a6ec990a46c0e99dfe290e1cb
dariocostanzo <darioc@xara.com>
2020-05-13 15:20:56 +0100

    Using the token

--- store/actions/order.js
+++ store/actions/order.js
@@ -35,10 +35,11 @@ export const fetchOrders = () => {
 };
 
 export const addOrder = (cartItems, totalAmount) => {
-  return async dispatch => {
+  return async (dispatch, getState) => {
+    const token = getState().auth.token;
     const date = new Date();
     const response = await fetch(
-      'https://rn-shop-app-9c5fd.firebaseio.com/orders/u1.json',
+      `https://rn-shop-app-9c5fd.firebaseio.com/orders/u1.json?auth=${token}`,
       {
         method: 'POST',
         headers: {
---
+++ store/reducers/auth.js
@@ -0,0 +1,24 @@
+// To utilise the token
+import { LOGIN, SIGNUP } from '../actions/auth';
+
+const initialState = {
+  token: null,
+  userId: null
+};
+//export the reducer function
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case LOGIN:
+      return {
+        token: action.token,
+        userId: action.userId
+      };
+    case SIGNUP:
+      return {
+        token: action.token,
+        userId: action.userId
+      };
+    default:
+      return state;
+  }
+};
